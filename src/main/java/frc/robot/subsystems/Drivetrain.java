ab709f572a6076b1be08e0ac89044e6729f092dc
Kevin-OConnor <koconnor@usfirst.org>
2023-10-17 16:58:14 -0400

    Switch gyro to NavX

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 
 
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -12,6 +14,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -29,9 +32,10 @@ public class Drivetrain extends SubsystemBase {
 
     SwerveDriveOdometry odometry;
 
-    Gyro gyro;
+    AHRS gyro;
 
     public Drivetrain() {
+        gyro = new AHRS(Port.kMXP);
         odometry = new SwerveDriveOdometry(
                 DriveConstants.kDriveKinematics, 
                 gyro.getRotation2d(), 
